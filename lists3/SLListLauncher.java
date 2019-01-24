c0ffa2cf50a3b77b49c2d32126117be42297da97
aviatesk <aviatesk@gmail.com>
2019-01-24 15:07:29 +0900

    Finish Lec.5

--- lists3/SLListLauncher.java
+++ lists3/SLListLauncher.java
@@ -1,6 +1,7 @@
 public class SLListLauncher {
 	public static void main(String[] args) {
-		SLList<String> s1 = new SLList<>("bone");
-		s1.addFirst("thugs");	
+		SLList<String> s1 = new SLList<String>("bone");
+		s1.addFirst("thugs");
+		System.out.println(s1.getFirst());
 	}
-} 
+}
